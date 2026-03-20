44f07686c83572fc62d55be9ba9f1146a6fc2c48
Pradeep lodhi <lodhipradeep948@gmail.com>
2026-03-20 20:55:14 +0530

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,8 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
-
+// export const ServerUrl  = "http://localhost:8000"
+ export const ServerUrl="https://ai-interview-web-application-2.onrender.com"
 function App() {
 
   const dispatch = useDispatch()

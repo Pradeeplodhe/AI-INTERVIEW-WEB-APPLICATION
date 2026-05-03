77d2012fb78cd2170d2604a2b9630892e10275c3
Your Name <you@example.com>
2026-05-03 21:09:03 +0530

    fix errors for vercel deploy

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,12 +3,11 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interview2-82ff3.firebaseapp.com",
-  projectId: "interview2-82ff3",
-  storageBucket: "interview2-82ff3.firebasestorage.app",
-  messagingSenderId: "87208966711",
-  appId: "1:87208966711:web:7b10eb6c6c55eb34fc5db2",
-  measurementId: "G-GHDJRPX7NH"
+  authDomain: "interviewiq-12ced.firebaseapp.com",
+  projectId: "interviewiq-12ced",
+  storageBucket: "interviewiq-12ced.appspot.com",
+  messagingSenderId: "862159592601",
+  appId: "1:862159592601:web:7308d702cd708076ddec08"
 };
 
 const app = initializeApp(firebaseConfig);

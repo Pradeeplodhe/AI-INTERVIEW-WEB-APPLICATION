141b05bf9e99593ae075e9280f8da996755af798
Your Name <you@example.com>
2026-05-03 19:44:37 +0530

    firebase fix

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -27,24 +27,43 @@
 
 
 
+// import { initializeApp } from "firebase/app";
+// import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
+//   authDomain: "interview2-82ff3.firebaseapp.com",
+//   projectId: "interview2-82ff3",
+//   storageBucket: "interview2-82ff3.firebasestorage.app",
+//   messagingSenderId: "87208966711",
+//   appId: "1:87208966711:web:7b10eb6c6c55eb34fc5db2",
+//   measurementId: "G-GHDJRPX7NH"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+
+//  const auth = getAuth(app);
+//  const provider = new GoogleAuthProvider()
+//  export {auth , provider}
+
+
+
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
-
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: "interview2-82ff3.firebaseapp.com",
   projectId: "interview2-82ff3",
-  storageBucket: "interview2-82ff3.firebasestorage.app",
+  storageBucket: "interview2-82ff3.appspot.com", // 🔥 FIXED
   messagingSenderId: "87208966711",
   appId: "1:87208966711:web:7b10eb6c6c55eb34fc5db2",
-  measurementId: "G-GHDJRPX7NH"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
- const auth = getAuth(app);
- const provider = new GoogleAuthProvider()
- export {auth , provider}
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
